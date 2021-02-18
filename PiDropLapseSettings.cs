d928aacd710b75df7912d44092777c47e890c664
Charles Miles <charles@cmiles.info>
2021-02-17 18:59:40 -0700

    Ini File, Camera Setting, Control Drawn Text Width

---
+++ PiDropLapseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace PiDropLapse
+{
+    public class PiDropLapseSettings
+    {
+        public string DropboxAccessToken { get; set; } = string.Empty;
+        public int ExposureCompensation { get; set; }
+        public string FileIdentifierName { get; set; } = "Drop";
+        public int Iso { get; set; } = 0;
+        public int LongEdgeResolution { get; set; } = 1280;
+        public int Rotation { get; set; } = 0;
+        public int ShutterSpeed { get; set; } = 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿#nullable enable
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Dropbox.Api;
 using Dropbox.Api.Files;
 using MMALSharp;
 using MMALSharp.Common;
+using MMALSharp.Common.Utility;
 using MMALSharp.Handlers;
+using SharpConfig;
 
 namespace PiDropLapse
 {
@@ -15,23 +20,92 @@ namespace PiDropLapse
     {
         private static async Task Main(string[] args)
         {
-            var executionTime = DateTime.Now;
+            //Simple parsing for help in the args
+            if (args.Length > 0 && args.Any(x => x.ToLower().Contains("help")))
+            {
+                Console.WriteLine("PiDropLapse Help");
+                Console.WriteLine("When you execute PiDropLapse it will attempt to:");
+                Console.WriteLine(" - Take a photo and save it in a subdirectory called 'Drops'");
+                Console.WriteLine(" - Upload the File to Dropbox");
 
-            Console.WriteLine($"Starting PiDropLapse {executionTime:yyyy-MM-dd-HH-mm-ss}");
+                Console.WriteLine();
+                Console.WriteLine("Settings are pulled from PiDropLapse.ini");
+                Console.WriteLine(
+                    " - If the program doesn't find the ini file when in runs a new one will be generated");
+                Console.WriteLine(" - Run PiDropLapse -WriteIni to write out a new ini file");
+                Console.WriteLine(" - For a Dropbox Upload to happen you need a valid AccessToken in the ini file");
+                Console.WriteLine(" - Some Camera Settings can be adjusted in the ini file");
+                Console.WriteLine(" - Set this up with cron to take a series of photos");
+
+                Console.WriteLine();
+                Console.WriteLine(
+                    "Created by Charles Miles - see https://github.com/cmiles/PiDropLapse for more information!");
+
+                return;
+            }
+
+            //Setup the config file
+            var configFile = new FileInfo(Path.Combine(AppContext.BaseDirectory, "PiDropLapse.ini"));
+
+            //Simple parsing for writeini in the args
+            if (args.Length > 0 && args.Any(x => x.ToLower().Contains("writeini")))
+            {
+                if (configFile.Exists)
+                {
+                    Console.WriteLine(
+                        $"Sorry - Won't overwrite an existing config - delete or rename {configFile.FullName} and then use -WriteIni again to create a fresh settings file.");
+                    return;
+                }
+
+                Console.WriteLine($"Writing New Settings File to {configFile.FullName}...");
+                var setupConfig = new Configuration {Section.FromObject("Main", new PiDropLapseSettings())};
+                setupConfig.SaveToFile(configFile.FullName);
+                return;
+            }
+
+
+            var executionTime = DateTime.Now;
+            Console.WriteLine($"Starting PiDropLapse Starting - {executionTime:yyyy-MM-dd-HH-mm-ss}");
+
+
+            Console.WriteLine($"Getting Config File - {configFile.FullName}");
+            if (!configFile.Exists)
+            {
+                Console.WriteLine("No Config File Found - writing Defaults");
+                var setupConfig = new Configuration {Section.FromObject("Main", new PiDropLapseSettings())};
+                setupConfig.SaveToFile(configFile.FullName);
+            }
+
+
+            Console.WriteLine("Loading Config File");
+            var configInformation = Configuration.LoadFromFile(configFile.FullName);
+            PiDropLapseSettings config;
+            try
+            {
+                Console.WriteLine("Parsing Config File");
+                config = configInformation["Main"].ToObject<PiDropLapseSettings>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Trouble Parsing Config - Using Defaults... Error {e}");
+                config = new PiDropLapseSettings();
+            }
+
+            Console.WriteLine("Config:");
+            Console.WriteLine(ObjectDumper.Dump(config, new DumpOptions {ExcludeProperties = new List<string> { "DropboxAccessToken" }, DumpStyle = DumpStyle.Console}));
+
 
             var targetDirectory = new DirectoryInfo(Path.Combine(AppContext.BaseDirectory, "Drops"));
-
             Console.WriteLine($"Local photo directory: {targetDirectory.FullName}");
-
             if (!targetDirectory.Exists) targetDirectory.Create();
 
-            var filePrefix = (DateTime.MaxValue - DateTime.Now).TotalHours.ToString("00000000");
 
+            var filePrefix = (DateTime.MaxValue - DateTime.Now).TotalHours.ToString("00000000");
             Console.WriteLine($"Photo prefix {filePrefix}");
 
-            var targetFile = new FileInfo(Path.Combine(targetDirectory.FullName,
-                $"{filePrefix}--Drop-{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.jpg"));
 
+            var targetFile = new FileInfo(Path.Combine(targetDirectory.FullName,
+                $"{filePrefix}--{config.FileIdentifierName}--{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.jpg"));
             if (targetFile.Exists)
             {
                 Console.WriteLine("Deleting Previous Photo");
@@ -39,67 +113,76 @@ namespace PiDropLapse
                 targetFile.Refresh();
             }
 
-            Console.WriteLine("Taking Photo");
 
-            // Singleton initialized lazily. Reference once in your application.
-            MMALCamera cam = MMALCamera.Instance;
+            Console.WriteLine("Initializing Camera");
+            MMALCamera piCamera = MMALCamera.Instance;
+            MMALCameraConfig.ISO = config.Iso;
+            MMALCameraConfig.ShutterSpeed = config.ShutterSpeed;
+            MMALCameraConfig.Rotation = config.Rotation;
+            MMALCameraConfig.ExposureCompensation = config.ExposureCompensation;
+            if (MMALCameraConfig.Rotation == 0 || MMALCameraConfig.Rotation == 180)
+                MMALCameraConfig.StillResolution = new Resolution(config.LongEdgeResolution,
+                    config.LongEdgeResolution / 16 * 9);
+            if (MMALCameraConfig.Rotation == 90 || MMALCameraConfig.Rotation == 270)
+                MMALCameraConfig.StillResolution =
+                    new Resolution(config.LongEdgeResolution / 16 * 9, config.LongEdgeResolution);
+
 
             using (var imgCaptureHandler = new ImageStreamCaptureHandler(targetFile.FullName))
             {
-                await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+                piCamera.ConfigureCameraSettings(imgCaptureHandler);
+                Console.WriteLine("Taking Photo");
+                await piCamera.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
             }
 
             Console.WriteLine("Cleaning Up");
+            piCamera.Cleanup();
 
-            // Cleanup disposes all unmanaged resources and unloads Broadcom library. To be called when no more processing is to be done
-            // on the camera.
-            cam.Cleanup();
 
+            Console.WriteLine("Loading file to write date");
             Image bmp;
 
+            //Read the photo, delete it - draw the text on it and save it
             await using (FileStream fs = new(targetFile.FullName, FileMode.Open))
             {
                 bmp = Image.FromStream(fs);
                 fs.Close();
             }
-            
+
             targetFile.Delete();
 
-            Graphics gra = Graphics.FromImage(bmp);
+            //Write the date into the top left of the photo using approximately 1/3
+            //of the width of the photo or a minimum of Verdana 12
+
+            Console.WriteLine("Writing date");
+            Graphics g = Graphics.FromImage(bmp);
+            var width = (int) g.VisibleClipBounds.Width;
+            Console.WriteLine($"Photo Width {width}");
 
             string dateText = executionTime.ToString("yyyy-MM-dd HH:mm:ss");
-            gra.DrawString(dateText, new Font("Verdana", 48), Brushes.Red, new PointF(20, 20));
+            var adjustedFont = TryAdjustFontSizeToFitWidth(g, dateText, new Font("Verdana", 12), width / 3, 12, 128);
+            Console.WriteLine($"Adjusted Font Size - {adjustedFont.Size}");
+            g.DrawString(dateText, adjustedFont, Brushes.Red, new PointF(20, 20));
+
+            Console.WriteLine("Saving file with date written");
             bmp.Save(targetFile.FullName);
-
             targetFile.Refresh();
-
             Console.WriteLine($"{targetFile.FullName} -- File Length {targetFile.Length}");
 
-            var accessTokenFile = new FileInfo(Path.Combine(AppContext.BaseDirectory, "PiDropLapseToken.txt"));
 
-            if (!accessTokenFile.Exists)
+            //If we have a DropboxAccessToken upload to Dropbox
+            if (string.IsNullOrWhiteSpace(config.DropboxAccessToken))
             {
-                Console.WriteLine($"Did not find {accessTokenFile.FullName} - skipping Dropbox processing and ending...");
+                Console.WriteLine("Did not find a DropboxAccessToken - skipping Dropbox processing and ending...");
                 return;
             }
 
-            Console.WriteLine($"Found {accessTokenFile.FullName} - Starting Dropbox Processing");
-
-            var accessToken = await File.ReadAllTextAsync(accessTokenFile.FullName);
-
-            if (string.IsNullOrWhiteSpace(accessToken))
-            {
-                Console.WriteLine($"{accessTokenFile.FullName} was empty - ending...");
-                return;
-            }
-
-            Console.WriteLine("Starting Dropbox File List...");
+            Console.WriteLine("Found a Dropbox Access Token - Starting Dropbox Connection");
 
             DropboxClient dropClient;
-
             try
             {
-                dropClient = new DropboxClient(accessToken.Trim());
+                dropClient = new DropboxClient(config.DropboxAccessToken.Trim());
             }
             catch (Exception e)
             {
@@ -108,11 +191,43 @@ namespace PiDropLapse
             }
 
             Console.WriteLine("Starting Dropbox Upload...");
-
             var dropboxUploadResult =
                 await dropClient.Files.UploadAsync(new CommitInfo($@"/{targetFile.Name}"), targetFile.OpenRead());
-
             Console.WriteLine($"Dropbox File Uploaded {dropboxUploadResult.PathDisplay}");
+        }
+
+        /// <summary>
+        ///     Returns a font (inside the specified range) that is the largest font
+        ///     that will fit inside the given width.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="stringToDraw"></param>
+        /// <param name="referenceFont"></param>
+        /// <param name="maxWidth"></param>
+        /// <param name="minimumFontSize"></param>
+        /// <param name="maximumFontSize"></param>
+        /// <returns></returns>
+        public static Font TryAdjustFontSizeToFitWidth(Graphics g, string stringToDraw, Font referenceFont,
+            int maxWidth, int minimumFontSize, int maximumFontSize)
+        {
+            //Based on https://docs.microsoft.com/en-us/previous-versions/bb986765(v=msdn.10)?redirectedfrom=MSDN 
+            //with thanks to https://stackoverflow.com/questions/15571715/auto-resize-font-to-fit-rectangle/30567857 for the link;
+
+            Font testFont = new(referenceFont.Name, referenceFont.Size, referenceFont.Style);
+
+            for (var testSize = maximumFontSize; testSize >= minimumFontSize; testSize--)
+            {
+                testFont = new Font(referenceFont.Name, testSize, referenceFont.Style);
+
+                // Test the string with the new size
+                var adjustedSizeNew = g.MeasureString(stringToDraw, testFont);
+
+                if (maxWidth > Convert.ToInt32(adjustedSizeNew.Width))
+                    // First font to fit - return it
+                    return testFont;
+            }
+
+            return testFont;
         }
     }
 }
